0f69ecc88900977b89e05f2a92daab42a47cd4e5
Stephen Provis <me@stephenprovis.co.uk>
2015-09-09 18:28:20 +0100

    Update client.rb
    Changes

--- lib/brightpearl_api/client.rb
+++ lib/brightpearl_api/client.rb
@@ -35,7 +35,7 @@ module BrightpearlApi
         :headers => {
           'Content-Type' => 'application/json',
           'brightpearl-app-ref' => configuration.appref,
-          'brightpearl-staff-token' => configuration.apptoken,
+          'brightpearl-account-token' => configuration.apptoken,
           'Accept' => 'json',
           'brightpearl-auth' => token
         },
@@ -46,7 +46,7 @@ module BrightpearlApi
         :headers => {
           'Content-Type' => 'application/json',
           'brightpearl-app-ref' => configuration.appref,
-          'brightpearl-staff-token' => configuration.apptoken,
+          'brightpearl-account-token' => configuration.apptoken,
           'Accept' => 'json',
           'brightpearl-auth' => token
         },
@@ -82,7 +82,7 @@ module BrightpearlApi
         },
         :body => {
           'brightpearl-app-ref' => configuration.appref,
-          'brightpearl-staff-token' => configuration.apptoken,
+          'brightpearl-account-token' => configuration.apptoken,
           :apiAccountCredentials => {
             :emailAddress => configuration.email,
             :password => configuration.password
